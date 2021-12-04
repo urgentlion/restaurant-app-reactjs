801a2d9bb60cd201e87a1585b7840131a61603bd
urgentlion <urgentlioninfo@gmail.com>
2021-12-03 16:36:42 -0800

    fixing toggle button

--- client/src/components/misc/Navbar.js
+++ client/src/components/misc/Navbar.js
@@ -4,11 +4,17 @@ import './Navbar.css';
 import {Link} from 'react-router-dom';
 
 const Navbar = () => {
+
+    const toggleButtonHandler = () => {
+        console.log('wired');
+    };
+ 
     return(
         <div className="top">
         <div className="navbar padding card" style={{letterSpacing:4}}>
             <Link to="/" className="item button">Gourmet au Catering</Link>
-            <Link to="#" className="toggle-button">
+            <Link to="#" className="toggle-button" onClick={toggleButtonHandler}>
+                {/* https://ibaslogic.com/how-to-add-hamburger-menu-in-react/ */}
                 <span className="bar"></span>
                 <span className="bar"></span>
                 <span className="bar"></span>
