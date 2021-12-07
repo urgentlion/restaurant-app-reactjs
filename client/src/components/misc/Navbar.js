0f545cedd1a85374e4c92357e6714320a90d4d49
urgentlion <urgentlioninfo@gmail.com>
2021-12-06 18:27:52 -0800

    fixed toggle button

--- client/src/components/misc/Navbar.js
+++ client/src/components/misc/Navbar.js
@@ -1,31 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './Navbar.css';
 import {Link} from 'react-router-dom';
 
 const Navbar = () => {
-
-    const toggleButtonHandler = () => {
-        console.log('wired');
-    };
+    const [navOpen, setNavOpen] = useState(true);
  
     return(
         <div className="top">
-        <div className="navbar padding card" style={{letterSpacing:4}}>
-            <Link to="/" className="item button">Gourmet au Catering</Link>
-            <Link to="#" className="toggle-button" onClick={toggleButtonHandler}>
-                {/* https://ibaslogic.com/how-to-add-hamburger-menu-in-react/ */}
-                <span className="bar"></span>
-                <span className="bar"></span>
-                <span className="bar"></span>
-            </Link>
-            {/* links on the right of the navbar */}
-            <div className="right-item-button">
-                <Link to="/about" className="item button">About</Link>
-                <Link to="/menu" className="item button">Menu</Link>
-                <Link to="/contact" className="item button">Contact</Link>
+            <div className="navbar" style={{letterSpacing:4}}>
+                {/* home link on the left of the navbar */}
+                <Link to="/" className="home-link">Gourmet au Catering</Link>
+                {/* button on the right of the navbar */}
+                <button className="toggle-button" onClick={() => setNavOpen(!navOpen)}> 
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </button>
+                {/* links on the right of the navbar */}
+                <div className="right-item-buttons" id={navOpen ? "hidden" : ""}>
+                    <Link to="/about" className="item">About</Link>
+                    <Link to="/menu" className="item">Menu</Link>
+                    <Link to="/contact" className="item">Contact</Link>
+                </div>
+                
             </div>
-        </div>
         </div>
     );
 };
